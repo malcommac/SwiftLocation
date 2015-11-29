6f60f04a333a4498c80caae356669629bef6c5d1
Andrea Mazzini <andrea.mazzini@gmail.com>
2015-11-29 17:17:00 +0100

    Add Carthage support

---
+++ SwiftLocation/SwiftLocation.h
@@ -0,0 +1,19 @@
+//
+//  SwiftLocation.h
+//  SwiftLocation
+//
+//  Created by Andrea Mazzini on 29/11/15.
+//  Copyright © 2015 danielemargutti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftLocation.
+FOUNDATION_EXPORT double SwiftLocationVersionNumber;
+
+//! Project version string for SwiftLocation.
+FOUNDATION_EXPORT const unsigned char SwiftLocationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLocation/PublicHeader.h>
+
+
