1d980f83d23399e508ba0d475edbfc120d103652
Phong Le <phongleq@gmail.com>
2019-11-08 13:56:22 -0700

    Corrected the minimum

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftLocation",
     platforms: [
-        .iOS(.v9)
+        .iOS("9.3")
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
