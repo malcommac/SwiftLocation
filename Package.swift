2674f0f0fe3f3d880e922e34b452b9aa74ff8ff0
Daniele Margutti <hello@danielemargutti.com>
2021-02-10 10:59:00 +0100

    Removed CoreLocation from SwiftPM target

--- Package.swift
+++ Package.swift
@@ -38,7 +38,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftLocation",
-            dependencies: ["CoreLocation"],
+            dependencies: [],
             path: "Sources/SwiftLocation"
         ),
         .target(
@@ -48,7 +48,7 @@ let package = Package(
         ),
         .testTarget(
             name: "SwiftLocationTests",
-            dependencies: ["SwiftLocation"]
+            dependencies: ["SwiftLocation","SwiftLocationBeaconBroadcaster"]
         )
     ]
 )
